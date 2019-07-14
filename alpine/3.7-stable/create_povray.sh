4ec773e597b0966b6db8f9dc9cec2f896fc8fab0
Oliver Cordes <ocordes@astro.uni-bonn.de>
2019-07-14 18:50:08 +0200

    - fix a BUG in create_povray.sh script

--- alpine/3.7-stable/create_povray.sh
+++ alpine/3.7-stable/create_povray.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for arch in "core2 nehalem westmere sandybridge ivybridge haswell broadwell skylake knl knm silvermont bonell athlon64 athlon64-sse3 amdfam10 btver1 btver2 bdver1 bdver2 znver1 x86-64"; do
+for arch in core2 nehalem westmere sandybridge ivybridge haswell broadwell skylake knl knm silvermont bonell athlon64 athlon64-sse3 amdfam10 btver1 btver2 bdver1 bdver2 znver1 x86-64 ; do
  ./configure COMPILED_BY="Oliver Cordes ocordes/povray:3.7-stable" \
       --disable-optimiz \
  CXXFLAGS="-O3 -march=${arch} -include uint-hack.h -I/usr/include/OpenEXR"
